a1a2134abe32ec47e932a7fb9c5b4e52c5d6e3da
Sayantan De <sayantande31@gmail.com>
2023-07-10 02:04:15 +0530

    change

--- Graph/src/Main.java
+++ Graph/src/Main.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static final String STRING = "--------------------";
-
     public static void main(String[] args) {
+        final String STRING = "--------------------";
 
-        GraphRepresentation graphRepresentation=new GraphRepresentation();
+            GraphRepresentation graphRepresentation=new GraphRepresentation();
         /*
                           ------------
                           |          |
@@ -13,30 +12,30 @@ public class Main {
                     |          |
                     ------------
         */
-        int[][] inputArr = {{1,2}, {2, 3}, {3, 4},{1,3},{2,4},{5,4}};
-        int n=5;
+            int[][] inputArr = {{1,2}, {2, 3}, {3, 4},{1,3},{2,4},{5,4}};
+            int n=5;
 
-        ArrayList<ArrayList<Integer>>  result = graphRepresentation.getAdjacencyList(inputArr,n);
-        System.out.println("\n1 BASED\n" + STRING);
-        for(int i=0;i<result.size();i++){
-            System.out.println(i+" -> "+result.get(i));
-        }
+            ArrayList<ArrayList<Integer>>  result = graphRepresentation.getAdjacencyList(inputArr,n);
+            System.out.println("\n1 BASED\n" + STRING);
+            for(int i=0;i<result.size();i++){
+                System.out.println(i+" -> "+result.get(i));
+            }
 
-                /*
+        /*
                           ------------
                           |          |
                     0-----1----2-----3----4
                     |          |
                     ------------
         */
-        int[][] inputArr1 = {{0,1}, {1, 2}, {2, 3},{0,2},{1,3},{3,4}};
-        int n1=5;
+            int[][] inputArr1 = {{0,1}, {1, 2}, {2, 3},{0,2},{1,3},{3,4}};
+            int n1=5;
 
-        ArrayList<ArrayList<Integer>>  result1 = graphRepresentation.getAdjacencyList(inputArr1,n1);
-        System.out.println("\n0 BASED\n" + STRING);
-        for(int i=0;i<result.size();i++){
-            System.out.println(i+" -> "+result1.get(i));
-        }
+            ArrayList<ArrayList<Integer>>  result1 = graphRepresentation.getAdjacencyList(inputArr1,n1);
+            System.out.println("\n0 BASED\n" + STRING);
+            for(int i=0;i<result.size();i++){
+                System.out.println(i+" -> "+result1.get(i));
+            }
 
     }
 }
